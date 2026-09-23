6c7373e24a7e3baa4fd28e02e9ebfdc95feb8996
eslambin-barek <eslam.bin-barek@stu.ocsb.ca>
2026-09-23 09:53:14 -0400

    New save

--- src/Main.java
+++ src/Main.java
@@ -27,14 +27,48 @@ public class Main {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		// code under---
-		int CaeserSalad = 6 ;
+		// code under---		 
+		
+		
+		
+		int CaeserSaladPrice = 6;
 		// print restaurant name.
 		System.out.println("Welcome to Eslam's Restaurant!");
 		// ask about 10$ Shawarma bowl
 		System.out.println("We serve our world famous 10$ Shawarma bowls as a main dish here. How much would you like?");
 		int num1 = in.nextInt();
-		System.out.println("We serve +CaeserSalad+ , ");
+		System.out.println("You ordered "+num1+"! that will be "+num1*10+"$. ");
+		System.out.println("We sell Caeser salads as a side dish for 5$. How many would you like?");
+		int num2 = in.nextInt();
+		System.out.println("You ordered "+num2+"! That will be "+num2*5+"$. ");
+		System.out.println("We sell drinks for a 2$ each. How much would you like?");
+		int num3 = in.nextInt();
+		System.out.println("You ordered "+num3+"! That will be "+num3*2+"$. ");
+		System.out.println("We sell an Egg Sandwhich for 3$ as well. How many of them would you like?");
+		int num4 = in.nextInt();
+		System.out.println("You ordered "+num4+"! That will be "+num4*3+"$. ");
+		System.out.println("We sell Ice cream as a dessert for 4$ as well. How many of them would you like?");
+		int num5 = in.nextInt();
+		System.out.println("You ordered "+num5+"! That will be "+num5*4+"$. ");
+		
+		double miniPrice1 = num1*10;
+		double miniPrice2 = num2*5;
+		double miniPrice3 = num3*2;
+		double miniPrice4 = num3*3;
+		double miniPrice5 = num5*4;
+		double Price = miniPrice1 + miniPrice2 + miniPrice3 + miniPrice4 + miniPrice5*1.13  ;
+
+		System.out.println("Your total price comes out to "+Price+"$ ");
+
+		System.out.println(Price > 30);
+		boolean SpecialPrice1 = true;
+		if (Price > 30) { 
+			  System.out.println("You got a 10% discount! Your new price is "+SpecialPrice1+" ");
+			} else {
+			  System.out.println("Thank you for choosing Eslam's Restaurant!");
+			}
+		if (Price > 50)
+			  System.out.println("You get a 20% discount!");
 	}
 
 }
